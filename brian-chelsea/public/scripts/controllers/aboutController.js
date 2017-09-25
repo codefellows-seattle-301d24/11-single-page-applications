214129128738a423212dc8a446cc31ccd992e0d2
Brian Wheeler <monsteraono@gmail.com>
2017-09-25 16:56:18 -0700

    completed controller.js todos

--- brian-chelsea/public/scripts/controllers/aboutController.js
+++ brian-chelsea/public/scripts/controllers/aboutController.js
@@ -4,8 +4,10 @@ var app = app || {};
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
-
+  // done: Define a function that hides all main section elements, and then reveals just the #about section:
+  function about(){
+    $('#articles').hide();
+    $('about').show();
+  }
   module.aboutController = aboutController;
 })(app);
