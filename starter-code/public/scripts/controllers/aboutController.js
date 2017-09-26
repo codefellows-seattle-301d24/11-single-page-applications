9fecb26593243d85b57c5ae4af85505ec178647d
Michael Shinners <mishinners@icloud.com>
2017-09-26 09:24:49 -0700

    completed 3 TODOs

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,8 +4,12 @@ var app = app || {};
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
+  function showAbout() {
 
-
+    $('main section').hide()
+    $('#about').show()
+  }
+  showAbout();
   module.aboutController = aboutController;
 })(app);
