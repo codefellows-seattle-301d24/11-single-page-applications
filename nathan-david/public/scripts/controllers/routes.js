9940a8d65e814f064dbec18838855f2b8a2c3865
Nathan Moore <ncmooore1986@gmail.com>
2017-09-26 08:55:04 -0700

    made page.js callbacks to handle the home route, the about route, and the anything else route.

--- nathan-david/public/scripts/controllers/routes.js
+++ nathan-david/public/scripts/controllers/routes.js
@@ -1,6 +1,20 @@
 'use strict';
 var app = app || {};
 
+page('/', () => {
+  $('main').hide();
+  $('#articles').fadeIn();
+});
+page('/about', () => {
+  $('main').hide();
+  $('#about').fadeIn();
+});
+page('*', () => {
+  $('main').hide();
+  $('main').append('<h1>No such page, my guy</h1>')
+});
+page();
+
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
 
 // TODO: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
