9d5ab0afb4efecef96a6a9d33ce3eed3b43ac804
Brian Wheeler <monsteraono@gmail.com>
2017-09-25 17:10:14 -0700

    fixed some issues and deleted for the one todo.

--- brian-chelsea/public/scripts/controllers/articleController.js
+++ brian-chelsea/public/scripts/controllers/articleController.js
@@ -6,7 +6,7 @@ var app = app || {};
   // done: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-  app.article.fetchAll(articleView.initIndexPage);
+  app.Article.fetchAll(app.articleView.initIndexPage);
   function articles(){
     $('#about').hide();
     $('#articles').show();
--- brian-chelsea/server.js
+++ brian-chelsea/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = ''; // TODO: Don't forget to set your own conString
+const conString = process.env.DATABASEURL || 'postgres://postgres:54321@localhost:5432/kilovolt'; // done: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
