d48a44f100e5709c4c3d3bdc63fd42f40e66143a
daniel <dpillay03@gmail.com>
2017-09-26 09:21:07 -0700

    initial commit

---
+++ daniel-kavdi/public/scripts/controllers/aboutController.js
@@ -0,0 +1,14 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  const aboutController = {};
+
+  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  function hideMain(){
+    $('#article-template').hide;
+    $('#about').show;
+  }
+
+  module.aboutController = aboutController;
+})(app);
