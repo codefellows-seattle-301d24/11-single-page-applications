ddafdb839ef167c3902788a664bc004fa8437ab1
CHELSEA DOLE <chelseadole@gmail.com>
2017-09-27 12:05:24 -0700

    last commit before resubmit

--- brian-chelsea/public/scripts/controllers/aboutController.js
+++ brian-chelsea/public/scripts/controllers/aboutController.js
@@ -2,12 +2,11 @@
 var app = app || {};
 
 (function(module) {
-  const aboutController = {};
 
   // done: Define a function that hides all main section elements, and then reveals just the #about section:
   function about(){
     $('#articles').hide();
-    $('about').show();
+    $('#about').show();
   }
-  module.aboutController = aboutController;
+  module.about = about;
 })(app);
